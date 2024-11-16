f1c8b49540091a738362944d6cf80ade826fdee0
Aleksey Nemesh <Grifon595@yandex.ru>
2024-11-16 23:57:42 +0300

    Open/close popup

--- scripts/cards.js
+++ scripts/cards.js
@@ -24,14 +24,3 @@ const initialCards = [
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     }
 ];
-
-const placesList = document.querySelector('.places__list');
-const cardTemplate = document.getElementById('card-template').content;
-
-initialCards.forEach((item) => {
-  const cardItem = cardTemplate.querySelector('li').cloneNode(true);
-  cardItem.querySelector('.card__image').setAttribute('src', item.link);
-  cardItem.querySelector('.card__image').setAttribute('alt', item.name);
-  cardItem.querySelector('.card__title').textContent = item.name;
-  placesList.appendChild(cardItem);
-});
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,33 @@
 // @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.getElementById('card-template').content;
+const profileEditButton = document.querySelector('.profile__edit-button');
+const popupTypeEdit = document.querySelector('.popup_type_edit');
+const popupClose = document.querySelector('.popup__close');
+
+
+initialCards.forEach((item) => {
+  const cardItem = cardTemplate.querySelector('li').cloneNode(true);
+  cardItem.querySelector('.card__image').setAttribute('src', item.link);
+  cardItem.querySelector('.card__image').setAttribute('alt', item.name);
+  cardItem.querySelector('.card__title').textContent = item.name;
+  placesList.appendChild(cardItem);
+});
+
+
+// Открытие окна для редактирования профиля
+profileEditButton.addEventListener('click', function(evt) {
+    popupTypeEdit.classList.add('popup_is-opened');
+
+});
+
+// Закрытие окна для редактирования профиля
+popupClose.addEventListener('click', function(evt) {
+    popupTypeEdit.classList.remove('popup_is-opened');
+});
+
+
+
 
 // @todo: DOM узлы
 
@@ -7,3 +36,4 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+  
