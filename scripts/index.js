97b9f83876470b6655694babaafab9b893c85e0d
Aleksey Nemesh <Grifon595@yandex.ru>
2024-11-17 10:36:00 +0300

    AddLikes

--- scripts/index.js
+++ scripts/index.js
@@ -18,7 +18,7 @@ initialCards.forEach((item) => {
 // Открытие окна для редактирования профиля
 profileEditButton.addEventListener('click', function(evt) {
     popupTypeEdit.classList.add('popup_is-opened');
-
+    popupTypeEdit.classList.add('popup_is-animated');
 });
 
 // Закрытие окна для редактирования профиля
@@ -26,6 +26,37 @@ popupClose.addEventListener('click', function(evt) {
     popupTypeEdit.classList.remove('popup_is-opened');
 });
 
+// Добавление лайка
+placesList.addEventListener('click', function(evt) {
+    evt.target.classList.toggle('card__like-button_is-active');
+});
+
+// Находим форму в DOM
+const profileFormElement = document.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+const nameInput = document.querySelector('.popup__input_type_card-name');
+const jobInput = document.querySelector('.popup__input popup__input_type_url');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    console.log(nameInput.value);
+    // Выберите элементы, куда должны быть вставлены значения полей
+    const profileTitle = nameInput.value;
+    const profileDescription = jobInput.value;
+    // Вставьте новые значения с помощью textContent
+    profileTitle.textContent;
+    profileDescription.textContent;
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
 
 
 
